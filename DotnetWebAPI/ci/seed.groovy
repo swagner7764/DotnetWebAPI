f830308a1b4dbe04b99114f849cd8a7e2f852181
Sean <swagner7764@gmail.com>
2024-03-07 14:30:00 -0700

    updates to groovy

--- DotnetWebAPI/ci/seed.groovy
+++ DotnetWebAPI/ci/seed.groovy
@@ -5,14 +5,15 @@ job('DotnetWebAPI/compile'){
         	github('swagner7764/DotnetWebAPI', 'master')
     	}
   	steps{
-      		powerShell 'nuget restore "$ENV:WORKSPACE"'
-		msBuild {
+      		msBuild {
 	            	msBuildInstallation('MSBuild 2022')
-	            	buildFile('${WORKSPACE}/DotnetWebAPI.sln')	            	
-        	}			
+	            	buildFile('${WORKSPACE}/DotnetWebAPI.sln')	
+                    args('-restore')
+        	}	
+            
    	}    
   	publishers {
-        	downstream 'DotnetWebAPI/dotne-containerize', 'SUCCESS'
+        	downstream 'DotnetWebAPI/containerize', 'SUCCESS'
    	}
 }
 
@@ -21,12 +22,12 @@ job('DotnetWebAPI/containerize'){
 	customWorkspace('C:/tools/jenkins-agent/workspace/DotnetWebAPI/compile')
 	label('Windows')
     	steps{
-		powerShell 'docker build . -t dotnettest:1.0.2.$ENV:BUILD_NUMBER -t dotnettest:latest -f C:/tools/jenkins-agent/workspace/DotnetConsoleApp/dotnet-compile/DotnetConsoleApp/DockerFile'
+		powerShell 'docker build . -t dotnetwebapi:1.0.2.$ENV:BUILD_NUMBER -t dotnetwebapi:latest -f C:/tools/jenkins-agent/workspace/DotnetConsoleApp/compile/DotnetWebAPI/DockerFile'
 		powerShell 'aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 105414332808.dkr.ecr.us-west-1.amazonaws.com'
-		powerShell 'docker tag dotnettest:1.0.2.$ENV:BUILD_NUMBER 105414332808.dkr.ecr.us-west-1.amazonaws.com/dotnettest:1.0.2.$ENV:BUILD_NUMBER'
-		powerShell 'docker push 105414332808.dkr.ecr.us-west-1.amazonaws.com/dotnettest:1.0.2.$ENV:BUILD_NUMBER'
-		powerShell 'docker tag dotnettest:1.0.2.$ENV:BUILD_NUMBER 105414332808.dkr.ecr.us-west-1.amazonaws.com/dotnettest:latest'
-		powerShell 'docker push 105414332808.dkr.ecr.us-west-1.amazonaws.com/dotnettest:latest'
+		powerShell 'docker tag dotnetwebapi:1.0.2.$ENV:BUILD_NUMBER 105414332808.dkr.ecr.us-west-1.amazonaws.com/dotnetwebapi:1.0.2.$ENV:BUILD_NUMBER'
+		powerShell 'docker push 105414332808.dkr.ecr.us-west-1.amazonaws.com/dotnetwebapi:1.0.2.$ENV:BUILD_NUMBER'
+		powerShell 'docker tag dotnetwebapi:1.0.2.$ENV:BUILD_NUMBER 105414332808.dkr.ecr.us-west-1.amazonaws.com/dotnetwebapi:latest'
+		powerShell 'docker push 105414332808.dkr.ecr.us-west-1.amazonaws.com/dotnetwebapi:latest'
 	}  	
 }
 
